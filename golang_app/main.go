5b1aa14f46d7151e957ede5da68a76cec014611f
mustafaibrahim <swe.mustafaibrahim@gmail.com>
2025-04-27 22:26:47 +0300

    adding bad code

--- golang_app/main.go
+++ golang_app/main.go
@@ -1,19 +1,15 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/fatih/color"
-)
-
-var (
-	red = color.New(color.FgRed).SprintFunc()
-)
+import "fmt"
 
 func main() {
 	fmt.Println(hello("GitHub"))
 }
 
 func hello(name string) (statement string) {
-	return fmt.Sprintf("Hello, %s.", red(name))
+	return fmt.Sprintf("Hello, %s.", name)
+}
+
+func badFunc() {
+	fmt.Println("I'm not going to get used.")
 }
